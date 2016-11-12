e0b5a92b53ca3317740589bb65dd79d7fbc8d3c8
Erick Hitter <services@ethitter.com>
2016-11-11 20:44:01 -0800

    Prepare for tests of internal events

---
+++ tests/test-internal-events.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class WPCCR_Internal_Events_Test
+ *
+ * @package WP_Cron_Control_Revisited
+ */
+
+/**
+ * Sample test case.
+ */
+class WPCCR_Internal_Events_Test extends WP_UnitTestCase {
+
+	/**
+	 * Internal events should be scheduled
+	 */
+	function test_events() {
+		$this->assertTrue( true );
+	}
+}
