839dde7d11a46c67afbad6d930f8f0f0eaf56235
Erick Hitter <services@ethitter.com>
2016-11-11 16:16:00 -0800

    Introduce basic test framework using what WP-CLI generates

---
+++ tests/test-rest-api.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class WPCCR_REST_API_Test
+ *
+ * @package WP_Cron_Control_Revisited
+ */
+
+/**
+ * Sample test case.
+ */
+class WPCCR_REST_API_Test extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_events_list_endpoint() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
