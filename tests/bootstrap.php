ce26c87c04d4a398aa51d8303f9dfd419d896a71
Erick Hitter <services@ethitter.com>
2016-11-11 17:30:08 -0800

    Define the constant this plugin needs to actually do anything

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,6 +17,8 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
+	define( 'WP_CRON_CONTROL_SECRET', 'testtesttest' );
+
 	require dirname( dirname( __FILE__ ) ) . '/wp-cron-control-revisited.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
