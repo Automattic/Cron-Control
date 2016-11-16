9626da5a034e2d0b76114447f2a1d566195bfdba
Erick Hitter <services@ethitter.com>
2016-11-15 19:25:57 -0800

    Basic tests that internal events are scheduled properly

--- tests/test-internal-events.php
+++ tests/test-internal-events.php
@@ -14,6 +14,16 @@ class WPCCR_Internal_Events_Test extends WP_UnitTestCase {
 	 * Internal events should be scheduled
 	 */
 	function test_events() {
-		$this->assertTrue( true );
+		WP_Cron_Control_Revisited\Internal_Events::instance()->schedule_internal_events();
+
+		$events = \WP_Cron_Control_Revisited\collapse_events_array( get_option( 'cron' ) );
+
+		// Check that the plugin scheduled the expected number of events
+		$this->assertEquals( count( $events ), 3 );
+
+		// Confirm that the scheduled jobs came from the Internal Events class
+		foreach ( $events as $event ) {
+			$this->assertTrue( \WP_Cron_Control_Revisited\is_internal_event( $event['action'] ) );
+		}
 	}
 }
