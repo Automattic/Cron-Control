6f5a57c34766d92a98e4cf60ea8f276f79e6d526
Erick Hitter <services@ethitter.com>
2016-11-15 17:25:25 -0800

    Improve utility added in f347150 to allow for selecting specific timestamps

--- includes/utils.php
+++ includes/utils.php
@@ -7,13 +7,26 @@ namespace WP_Cron_Control_Revisited;
  *
  * Also removes superfluous, non-event data that Core stores in the option
  */
-function collapse_events_array( $events ) {
+function collapse_events_array( $events, $timestamp = null ) {
 	$collapsed_events = array();
 
+	// Ensure an event is always returned
 	if ( ! is_array( $events ) ) {
 		return $collapsed_events;
 	}
 
+	// Allow filtering to only events with a given timestamp
+	if ( is_numeric( $timestamp ) ) {
+		if ( isset( $events[ $timestamp ] ) ) {
+			$_events = $events[ $timestamp ];
+			$events  = array( $timestamp => $_events, );
+			unset( $_events );
+		} else {
+			return $collapsed_events;
+		}
+	}
+
+	// Collapse whatever events we have into an easier format to deal with
 	foreach ( $events as $timestamp => $timestamp_events ) {
 		// Skip non-event data that Core includes in the option
 		if ( ! is_numeric( $timestamp ) ) {
