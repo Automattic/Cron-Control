Automattic/Cron-Control

commit ad4a542061dc5e0611d6659ccd04dfe316cb8f8a

CLI: Error when requesting a non-existent page of events
Also stops the command from displaying an empty table if there are no events for the page

Fixes #67

includes/wp-cli/class-events.php
@@ -17,6 +17,11 @@ class Events extends \WP_CLI_Command {
 	public function list_events( $args, $assoc_args ) {
 		$events = $this->get_events( $args, $assoc_args );
 
+		// Prevent one from requesting a page that doesn't exist
+		if ( $events['page'] > $events['total_pages'] ) {
+			\WP_CLI::error( __( 'Invalid page requested', 'automattic-cron-control' ) );
+		}
+
 		// Output in the requested format
 		if ( isset( $assoc_args['format'] ) && 'ids' === $assoc_args['format'] ) {
 			echo implode( ' ', wp_list_pluck( $events['items'], 'ID' ) );
@@ -27,8 +32,8 @@ class Events extends \WP_CLI_Command {
 			}
 
 			// Not much to do
-			if ( 0 === $events['total_items'] ) {
-				\WP_CLI::success( __( 'No events to display', 'automattic-cron-control' ) );
+			if ( 0 === $events['total_items'] || empty( $events['items'] ) ) {
+				\WP_CLI::warning( __( 'No events to display', 'automattic-cron-control' ) );
 				return;
 			}
 

commit 77b70d7235555e1d292fdc9eb1ba676c62756a82

Requesting the first page, even of any empty set, shouldn't be considered an error
Corrects a bug introduced in ad4a542
Fixes #67

includes/wp-cli/class-events.php
@@ -18,7 +18,8 @@ class Events extends \WP_CLI_Command {
 		$events = $this->get_events( $args, $assoc_args );
 
 		// Prevent one from requesting a page that doesn't exist
-		if ( $events['page'] > $events['total_pages'] ) {
+		// Shouldn't error when first page is requested, though, as that is handled below and is an odd behaviour otherwise
+		if ( $events['page'] > $events['total_pages'] && $events['page'] > 1 ) {
 			\WP_CLI::error( __( 'Invalid page requested', 'automattic-cron-control' ) );
 		}
 