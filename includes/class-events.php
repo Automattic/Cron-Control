Automattic/Cron-Control

commit 5337e4abf832238cf021fa7467b30314479cbc1d

Check the event-level lock first, ensuring Internal Events are locked at only the event level
Also avoids an event-level lock check failing, which would've wasted a space in the global lock for the current period.

Fixes #76

includes/class-events.php
@@ -293,18 +293,18 @@ class Events extends Singleton {
 	 * @return bool
 	 */
 	private function can_run_event( $event ) {
-		// Internal Events always run
+		// Limit to one concurrent execution of a specific action
+		if ( ! Lock::check_lock( $this->get_lock_key_for_event_action( $event ), 1, JOB_LOCK_EXPIRY_IN_MINUTES * \MINUTE_IN_SECONDS ) ) {
+			return false;
+		}
+
+		// Internal Events aren't subject to the global lock
 		if ( is_internal_event( $event['action'] ) ) {
 			return true;
 		}
 
 		// Check if any resources are available to execute this job
 		if ( ! Lock::check_lock( self::LOCK, JOB_CONCURRENCY_LIMIT ) ) {
-			return false;
-		}
-
-		// Limit to one concurrent execution of a specific action
-		if ( ! Lock::check_lock( $this->get_lock_key_for_event_action( $event ), 1, JOB_LOCK_EXPIRY_IN_MINUTES * \MINUTE_IN_SECONDS ) ) {
 			return false;
 		}
 

commit c3cb7afc46748d1c9075f0acb91e054c9a969699

Don't deadlock the individual-events lock when the global lock blocks an event
This bug was introduced by 5337e4a

includes/class-events.php
@@ -304,7 +304,9 @@ class Events extends Singleton {
 		}
 
 		// Check if any resources are available to execute this job
+		// If not, the indivdual-event lock must be freed, otherwise it's deadlocked until it times out
 		if ( ! Lock::check_lock( self::LOCK, JOB_CONCURRENCY_LIMIT ) ) {
+			$this->reset_event_lock( $event );
 			return false;
 		}
 
@@ -324,7 +326,18 @@ class Events extends Singleton {
 		}
 
 		// Reset individual event lock
-		Lock::reset_lock( $this->get_lock_key_for_event_action( $event ), JOB_LOCK_EXPIRY_IN_MINUTES * \MINUTE_IN_SECONDS );
+		$this->reset_event_lock( $event );
+	}
+
+	/**
+	 * Frees the lock for an individual event
+	 *
+	 * @param $event array Event data
+	 *
+	 * @return bool
+	 */
+	private function reset_event_lock( $event ) {
+		return Lock::reset_lock( $this->get_lock_key_for_event_action( $event ), JOB_LOCK_EXPIRY_IN_MINUTES * \MINUTE_IN_SECONDS );
 	}
 
 	/**