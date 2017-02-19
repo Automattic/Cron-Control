01abbee6316e60ea47eaa54f8d32e9fdb8455c8f
Erick Hitter <services@ethitter.com>
2017-02-18 20:20:02 -0800

    Stop loading so much of the plugin before we know we're ready
    Plugin could be half-activated, due to secret's absence, but Events Store would still capture data. As the rest of the plugin, notably its CLI commands, aren't loaded, this could store data into an unreachable hole. Also, with the introduction of a custom table, the table shouldn't be created when the environment isn't ready.

--- cron-control.php
+++ cron-control.php
@@ -11,13 +11,7 @@
 namespace Automattic\WP\Cron_Control;
 
 // Load basics needed to instantiate plugin
-require __DIR__ . '/includes/constants.php';
-require __DIR__ . '/includes/utils.php';
 require __DIR__ . '/includes/abstract-class-singleton.php';
-require __DIR__ . '/includes/class-lock.php';
-
-// For data consistency, ensure alternate data store is always loaded
-require __DIR__ . '/includes/class-events-store.php';
 
 // Instantiate main plugin class, which checks environment and loads remaining classes when appropriate
 require __DIR__ . '/includes/class-main.php';
