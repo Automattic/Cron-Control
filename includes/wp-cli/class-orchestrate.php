59f814118f6070f5ba988534aa7e3bc4189d24fd
Erick Hitter <git-contrib@ethitter.com>
2017-07-04 20:29:49 -0700

    Endpoints to list events due now, curated

--- includes/wp-cli/class-orchestrate.php
+++ includes/wp-cli/class-orchestrate.php
@@ -2,19 +2,27 @@
 
 namespace Automattic\WP\Cron_Control\CLI;
 
-use Automattic\WP\Cron_Control;
-
 /**
  *
  */
 class Orchestrate extends \WP_CLI_Command {
 	/**
+	 * List the next set of events to run
 	 *
+	 * Will not be all events, just those atop the curated queue
 	 *
-	 * @subcommand do
+	 * @subcommand list-due-batch
 	 */
-	public function do_orchestration( $args, $assoc_args ) {
-		\WP_CLI::log( 'Hi' );
+	public function list_due_now( $args, $assoc_args ) {
+		$events = \Automattic\WP\Cron_Control\Events::instance()->get_events();
+
+		$format = \WP_CLI\Utils\get_flag_value( $assoc_args, 'format', 'table' );
+
+		\WP_CLI\Utils\format_items( $format, $events['events'], array(
+			'timestamp',
+			'action',
+			'instance',
+		) );
 	}
 }
 
