e3d0a19872e67fc3b9f84f9ec4b47ca9d968a57b
Erick Hitter <git-contrib@ethitter.com>
2017-08-01 21:42:57 -0700

    Tests for single-concurrency lock
    See #3

---
+++ tests/test-lock.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Class Lock_Tests
+ *
+ * @package Automattic_Cron_Control
+ */
+
+namespace Automattic\WP\Cron_Control\Tests;
+
+use Automattic\WP\Cron_Control;
+
+/**
+ * Lock tests
+ */
+class Lock_Tests extends \WP_UnitTestCase {
+	/**
+	 * Prepare test environment
+	 */
+	function setUp() {
+		parent::setUp();
+	}
+
+	/**
+	 * Clean up after our tests
+	 */
+	function tearDown() {
+		parent::tearDown();
+	}
+
+	/**
+	 * Test a single-concurrency lock
+	 */
+	function test_single_lock() {
+		$lock_name = 'test-lock';
+
+		Cron_Control\Lock::prime_lock( $lock_name );
+
+		$should_be_free = Cron_Control\Lock::check_lock( $lock_name, 1 );
+
+		$this->assertEquals( true, $should_be_free );
+
+		$should_be_locked = Cron_Control\Lock::check_lock( $lock_name, 1 );
+
+		$this->assertEquals( false, $should_be_locked );
+
+		Cron_Control\Lock::free_lock( $lock_name );
+
+		$should_be_free = Cron_Control\Lock::check_lock( $lock_name, 1 );
+
+		$this->assertEquals( true, $should_be_free );
+
+		Cron_Control\Lock::reset_lock( $lock_name );
+
+		$should_be_free = Cron_Control\Lock::check_lock( $lock_name, 1 );
+
+		$this->assertEquals( true, $should_be_free );
+	}
+}
