3cc39c57ce0b5013234fb1060549392594ddf27f
Erick Hitter <git-contrib@ethitter.com>
2017-08-03 12:03:22 -0700

    Split status check and status changes, to reduce magic in the command

--- includes/wp-cli/class-orchestrate.php
+++ includes/wp-cli/class-orchestrate.php
@@ -7,54 +7,62 @@ namespace Automattic\WP\Cron_Control\CLI;
  */
 class Orchestrate extends \WP_CLI_Command {
 	/**
-	 * Check and change status of automatic event execution
+	 * Check the status of automatic event execution
+	 *
+	 * @subcommand check-status
+	 */
+	public function get_automatic_execution_status( $args, $assoc_args ) {
+		$status = get_option( \Automattic\WP\Cron_Control\Events::DISABLE_RUN_OPTION, 0 );
+
+		switch ( $status ) {
+			case 0 :
+				$status = __( 'Automatic execution is enabled', 'automattic-cron-control' );
+				break;
+
+			case 1 :
+				$status = __( 'Automatic execution is disabled indefinitely', 'automattic-cron-control' );
+				break;
+
+			default :
+				$status = sprintf( __( 'Automatic execution is disabled until %s', 'automattic-cron-control' ), date_i18n( 'Y-m-d H:i:s T', $status ) );
+				break;
+		}
+
+		\WP_CLI::log( $status );
+	}
+
+	/**
+	 * Change status of automatic event execution
 	 *
 	 * When using the Go-based runner, it may be necessary to stop execution for a period, or indefinitely
 	 *
 	 * @subcommand manage-automatic-execution
 	 * @synopsis [--enable] [--disable] [--disable_until=<disable_until>]
 	 */
-	public function toggle_event_execution( $args, $assoc_args ) {
+	public function manage_automatic_execution( $args, $assoc_args ) {
 		// Update execution status
 		$disable_ts = \WP_CLI\Utils\get_flag_value( $assoc_args, 'disable_until', 0 );
 		$disable_ts = absint( $disable_ts );
 
 		if ( \WP_CLI\Utils\get_flag_value( $assoc_args, 'enable', false ) ) {
 			update_option( \Automattic\WP\Cron_Control\Events::DISABLE_RUN_OPTION, 0 );
-			\WP_CLI::success( 'Enabled' );
+			\WP_CLI::success( __( 'Enabled', 'automattic-cron-control' ) );
 			return;
 		} elseif ( \WP_CLI\Utils\get_flag_value( $assoc_args, 'disable', false ) ) {
 			update_option( \Automattic\WP\Cron_Control\Events::DISABLE_RUN_OPTION, 1 );
-			\WP_CLI::success( 'Disabled' );
+			\WP_CLI::success( __( 'Disabled', 'automattic-cron-control' ) );
 			return;
 		} elseif( $disable_ts > 0 ) {
 			if ( $disable_ts > time() ) {
 				update_option( \Automattic\WP\Cron_Control\Events::DISABLE_RUN_OPTION, $disable_ts );
-				\WP_CLI::success( sprintf( 'Disabled until %s', date( 'Y-m-d H:i:s T', $disable_ts ) ) );
+				\WP_CLI::success( sprintf( __( 'Disabled until %s', 'automattic-cron-control' ), date_i18n( 'Y-m-d H:i:s T', $disable_ts ) ) );
 				return;
 			} else {
-				\WP_CLI::error( 'Timestamp is in the past.' );
+				\WP_CLI::error( __( 'Timestamp is in the past.', 'automattic-cron-control' ) );
 			}
 		}
 
-		// Display existing status
-		$status = get_option( \Automattic\WP\Cron_Control\Events::DISABLE_RUN_OPTION, 0 );
-
-		switch ( $status ) {
-			case 0 :
-				$status = 'Automatic execution is enabled';
-				break;
-
-			case 1 :
-				$status = 'Automatic execution is disabled indefinitely';
-				break;
-
-			default :
-				$status = sprintf( 'Automatic execution is disabled until %s', date( 'Y-m-d H:i:s T', $status ) );
-				break;
-		}
-
-		\WP_CLI::log( $status );
+		\WP_CLI::error( __( 'Please provide a valid action.', 'automattic-cron-control' ) );
 	}
 }
 
