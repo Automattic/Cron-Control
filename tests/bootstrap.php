8ed0c38ef3ce84de3a97a45c316369e13f27cc79
Erick Hitter <git-contrib@ethitter.com>
2017-10-03 12:46:32 -0700

    Tests for event addition
    Removal of existing Internal Events is covered by the existing tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,6 +19,14 @@ require_once $_tests_dir . '/includes/functions.php';
 function _manually_load_plugin() {
 	define( 'WP_CRON_CONTROL_SECRET', 'testtesttest' );
 
+	define( 'CRON_CONTROL_ADDITIONAL_INTERNAL_EVENTS', array(
+		array(
+			'schedule' => 'hourly',
+			'action'   => 'cron_control_additional_internal_event',
+			'callback' => '__return_true',
+		),
+	) );
+
 	require dirname( dirname( __FILE__ ) ) . '/cron-control.php';
 
 	// Plugin loads after `wp_install()` is called, so we compensate.
