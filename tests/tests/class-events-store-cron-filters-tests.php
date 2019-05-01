df1740b005bfc2ce92b35772df2e86fddd962b35
Nick Daugherty <ndaugherty987@gmail.com>
2019-05-01 12:00:48 -0600

    phpcs fixes
    Since every comment is better with a period at the end!

--- tests/tests/class-events-store-cron-filters-tests.php
+++ tests/tests/class-events-store-cron-filters-tests.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Test Events Store's Cron Filters
  *
@@ -47,10 +46,10 @@ class Events_Store_Cron_Filter_Tests extends \WP_UnitTestCase {
 
 		$return_value = Events_Store_Cron_Filters::instance()->filter_pre_schedule_event( null, $event );
 
-		// Needs to return true to match wp_schedule_single_event()
+		// Needs to return true to match wp_schedule_single_event().
 		$this->assertEquals( true, $return_value );
 
-		// It should have been scheduled as expected
+		// It should have been scheduled as expected.
 		$scheduled_ts = wp_next_scheduled( $event->hook, $event->args );
 
 		$this->assertEquals( $event->timestamp, $scheduled_ts );
@@ -62,27 +61,27 @@ class Events_Store_Cron_Filter_Tests extends \WP_UnitTestCase {
 	function test_pre_reschedule_event_filter() {
 		$event = Utils::create_test_event();
 
-		// Ensure previous step actually scheduled it
+		// Ensure previous step actually scheduled it.
 		$scheduled_ts = wp_next_scheduled( $event['action'], $event['args'] );
 
 		$this->assertEquals( $event['timestamp'], $scheduled_ts );
 
-		// Reschedule it
+		// Reschedule it.
 		$event['schedule'] = 'daily';
 		$event['hook']     = $event['action']; // What's in a name, anyway?
 
 		$return_value = Events_Store_Cron_Filters::instance()->filter_pre_reschedule_event( null, (object) $event );
 
-		// Needs to return true to match wp_reschedule_hook()
+		// Needs to return true to match wp_reschedule_hook().
 		$this->assertEquals( true, $return_value );
 
-		// Unschedule the first, so we can more easily check the rescheduled one
+		// Unschedule the first, so we can more easily check the rescheduled one.
 		wp_unschedule_event( $event['timestamp'], $event['action'], $event['args'] );
 
-		// Now should have 1 scheduled
+		// Now should have 1 scheduled.
 		$scheduled_ts = wp_next_scheduled( $event['action'], $event['args'] );
 
-		// And it should be 1 day after the previous one
+		// And it should be 1 day after the previous one.
 		$expected_ts = $event['timestamp'] + \DAY_IN_SECONDS;
 
 		$this->assertEquals( $expected_ts, $scheduled_ts );
@@ -94,15 +93,15 @@ class Events_Store_Cron_Filter_Tests extends \WP_UnitTestCase {
 	function test_pre_unschedule_event_filter() {
 		$event = Utils::create_test_event();
 
-		// Ensure previous step actually scheduled it
+		// Ensure previous step actually scheduled it.
 		$scheduled_ts = wp_next_scheduled( $event['action'], $event['args'] );
 
 		$this->assertEquals( $event['timestamp'], $scheduled_ts );
 
-		// Now unschedule
+		// Now unschedule.
 		$return_value = Events_Store_Cron_Filters::instance()->filter_pre_unschedule_event( null, $event['timestamp'], $event['action'], $event['args'] );
 
-		// Needs to return true to match wp_unschedule_hook()
+		// Needs to return true to match wp_unschedule_hook().
 		$this->assertEquals( true, $return_value );
 
 		$scheduled_ts = wp_next_scheduled( $event['action'], $event['args'] );
@@ -116,13 +115,13 @@ class Events_Store_Cron_Filter_Tests extends \WP_UnitTestCase {
 	function test_pre_clear_scheduled_hook_filter() {
 		$event = Utils::create_test_event();
 
-		// Schedule a second with same info but 10s later (can't use util for this)
+		// Schedule a second with same info but 10s later (can't use util for this).
 		wp_schedule_single_event( $event['timestamp'] + 10, $event['action'], $event['args'] );
 
-		// Clear this hook
+		// Clear this hook.
 		$return_value = Events_Store_Cron_Filters::instance()->filter_pre_clear_scheduled_hook( null, $event['action'], $event['args'] );
 
-		// Needs to return the count of unscheduled hooks, to match wp_clear_scheduled_hook()
+		// Needs to return the count of unscheduled hooks, to match wp_clear_scheduled_hook().
 		$this->assertEquals( 2, $return_value );
 
 		$scheduled_ts = wp_next_scheduled( $event['action'], $event['args'] );
@@ -136,13 +135,13 @@ class Events_Store_Cron_Filter_Tests extends \WP_UnitTestCase {
 	function test_pre_unschedule_hook_filter() {
 		$event = Utils::create_test_event();
 
-		// Schedule a second but 10s later and with different args (can't use util for this)
+		// Schedule a second but 10s later and with different args (can't use util for this).
 		wp_schedule_single_event( $event['timestamp'] + 10, $event['action'], array( 'random' => rand( 0, 100 ) ) );
 
-		// Clear this hook
+		// Clear this hook.
 		$return_value = Events_Store_Cron_Filters::instance()->filter_pre_unschedule_hook( null, $event['action'] );
 
-		// Needs to return the count of unscheduled hooks, to match wp_unscheduled_hook()
+		// Needs to return the count of unscheduled hooks, to match wp_unscheduled_hook().
 		$this->assertEquals( 2, $return_value );
 
 		$scheduled_ts = wp_next_scheduled( $event['action'] );
@@ -156,7 +155,7 @@ class Events_Store_Cron_Filter_Tests extends \WP_UnitTestCase {
 	function test_pre_get_scheduled_event_filter() {
 		$event = Utils::create_test_event();
 
-		// Grab the scheduled event
+		// Grab the scheduled event.
 		$scheduled_event = Events_Store_Cron_Filters::instance()->filter_pre_get_scheduled_event( null, $event['action'], $event['args'], $event['timestamp'] );
 
 		$this->assertEquals( $event['action'], $scheduled_event->hook );
