7628e87b18bb8c66f28bd1a1da1011ab3f2e9c93
David Newman <darthhexx@gmail.com>
2019-07-24 23:31:18 +1000

    Add connection resume support
    * Logs all data to a file and streams data back to the TCP client.
    * If the connection drops, continue to write the WP-CLI command output to the file.
    * If the same Guid connects and the WP-CLI is still running, then reattach and send the catchup data and continue to stream new data.
    * Disconnects/reconnects can happen as many times as required.
    * Multiple TCP clinets can connect to the same running WP-CLI process.

--- runner/remote.go
+++ runner/remote.go
@@ -13,13 +13,29 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/creack/pty"
+	"github.com/howeyc/fsnotify"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+type WpCliProcess struct {
+	Guid          string
+	Cmd           *exec.Cmd
+	Tty           *os.File
+	Running       bool
+	LogFileName   string
+	BytesLogged   int64
+	BytesStreamed int64
+}
+
 var (
+	gGUIDttys map[string]*WpCliProcess
+	padlock   *sync.Mutex
+	guidRegex *regexp.Regexp
+
 	blackListed1stLevel = []string{"admin", "cli", "config", "core", "db", "dist-archive",
 		"eval-file", "eval", "find", "i18n", "scaffold", "server", "package", "profile"}
 
@@ -30,13 +46,12 @@ var (
 		"language": {"install", "update", "delete"},
 		"vip":      {"support-user"},
 	}
-
-	guidRegex *regexp.Regexp
-	padlock   *sync.Mutex
 )
 
 func waitForConnect() {
+	gGUIDttys = make(map[string]*WpCliProcess)
 	padlock = &sync.Mutex{}
+
 	guidRegex = regexp.MustCompile("^[a-fA-F0-9\\-]+$")
 	if nil == guidRegex {
 		logger.Println("Failed to compile the Guid regex")
@@ -147,6 +162,17 @@ func authConn(conn *net.TCPConn) {
 	conn.SetKeepAlivePeriod(time.Duration(30 * time.Second.Nanoseconds()))
 	conn.SetKeepAlive(true)
 
+	padlock.Lock()
+	wpCliProcess, found := gGUIDttys[Guid]
+	padlock.Unlock()
+
+	if found {
+		// Reattach to the running WP-CLi command
+		attachWpCliCmdRemote(conn, wpCliProcess, uint16(rows), uint16(cols))
+		return
+	}
+
+	// The Guid is not currently running
 	wpCliCmd, err := validateAndProcessCommand(elems[4])
 	if nil != err {
 		logger.Println(err.Error())
@@ -224,6 +250,195 @@ func getCleanWpCliArgumentArray(wpCliCmdString string) ([]string, error) {
 	return cleanArgs, nil
 }
 
+func processTCPConnectionData(conn *net.TCPConn, wpcli *WpCliProcess) {
+	data := make([]byte, 8192)
+	var size, written int
+	var err error
+
+	conn.SetReadBuffer(8192)
+	for {
+		size, err = (*conn).Read(data)
+
+		if nil != err {
+			if io.EOF == err {
+				logger.Println("client connection closed")
+			} else if !strings.Contains(err.Error(), "use of closed network connection") {
+				logger.Printf("error reading from the client connection: %s\n", err.Error())
+			}
+			break
+		}
+
+		if 0 == size {
+			logger.Println("ignoring data of length 0")
+			continue
+		}
+
+		if 1 == size && 0x3 == data[0] {
+			logger.Println("Ctrl-C received, terminating the WP-CLI and exiting")
+			wpcli.Cmd.Process.Kill()
+			break
+		}
+
+		if 4 < len(data) && "\xc2\x9b8;" == string(data[:4]) && 't' == data[size-1:][0] {
+			cmdParts := strings.Split(string(data[4:size]), ";")
+
+			rows, err := strconv.ParseUint(cmdParts[0], 10, 16)
+			if nil != err {
+				logger.Printf("error reading rows resize data from the WP CLI client: %s\n", err.Error())
+				continue
+			}
+			cols, err := strconv.ParseUint(cmdParts[1][:len(cmdParts[1])-1], 10, 16)
+			if nil != err {
+				logger.Printf("error reading columns resize data from the WP CLI client: %s\n", err.Error())
+				continue
+			}
+
+			err = pty.Setsize(wpcli.Tty, &pty.Winsize{Rows: uint16(rows), Cols: uint16(cols)})
+			if nil != err {
+				logger.Printf("error performing window resize: %s\n", err.Error())
+			} else {
+				logger.Printf("set new window size: %dx%d\n", rows, cols)
+			}
+			continue
+		}
+
+		written, err = wpcli.Tty.Write(data[:size])
+		if nil != err {
+			if io.EOF != err {
+				logger.Printf("error writing to the WP CLI tty: %s\n", err.Error())
+				break
+			}
+		}
+		if written != size {
+			logger.Println("error writing to the WP CLI tty: not enough data written")
+			break
+		}
+	}
+}
+
+func attachWpCliCmdRemote(conn *net.TCPConn, wpcli *WpCliProcess, rows uint16, cols uint16) error {
+	logger.Printf("resuming %s - rows: %d, cols: %d\n", wpcli.Guid, rows, cols)
+	connectionActive := true
+
+	err := pty.Setsize(wpcli.Tty, &pty.Winsize{Rows: uint16(rows), Cols: uint16(cols)})
+	if nil != err {
+		logger.Printf("error performing window resize: %s\n", err.Error())
+	} else {
+		logger.Printf("set new window size: %dx%d\n", rows, cols)
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		conn.Write([]byte("unable to reattach to the WP CLI processs"))
+		conn.Close()
+		return errors.New(fmt.Sprintf("error reattaching to the WP CLI process: %s\n", err.Error()))
+	}
+
+	go func() {
+		var written, read int
+		var buf []byte = make([]byte, 8192)
+
+		readFile, err := os.OpenFile(wpcli.LogFileName, os.O_RDONLY, os.ModeCharDevice)
+		if nil != err {
+			logger.Printf("error opening the read file for the catchup stream: %s\n", err.Error())
+			conn.Close()
+			return
+		}
+		defer readFile.Close()
+
+		logger.Printf("Seeking %s to %d for the catchup stream", wpcli.LogFileName, wpcli.BytesStreamed)
+		readFile.Seek(wpcli.BytesStreamed, 0)
+
+	Catchup_Loop:
+		for {
+			if !connectionActive {
+				logger.Println("client connection is closed, exiting this catchup loop")
+				break Catchup_Loop
+			}
+			read, err = readFile.Read(buf)
+
+			if nil != err {
+				logger.Printf("error reading file for the catchup stream: %s\n", err.Error())
+				break Catchup_Loop
+			}
+
+			if 0 == read {
+				logger.Printf("error reading file for the stream: %s\n", err.Error())
+				break Catchup_Loop
+			}
+
+			written, err = conn.Write(buf[:read])
+			if nil != err {
+				logger.Printf("error writing to client connection: %s\n", err.Error())
+				return
+			}
+
+			atomic.AddInt64(&wpcli.BytesStreamed, int64(written))
+
+			if wpcli.BytesStreamed == wpcli.BytesLogged {
+				break Catchup_Loop
+			}
+		}
+
+		// Used to monitor when the connection is disconnected or the CLI command finishes
+		ticker := time.Tick(time.Duration(1 * time.Second.Nanoseconds()))
+
+	Watcher_Loop:
+		for {
+			if !connectionActive || !wpcli.Running {
+				logger.Println("client connection is closed, exiting this watcher loop")
+				break Watcher_Loop
+			}
+			if !wpcli.Running {
+				logger.Println("WP CLI command finished, exiting this watcher loop")
+				break Watcher_Loop
+			}
+
+			select {
+			case <-ticker:
+				// We are just tick-tocking
+			case ev := <-watcher.Event:
+				if ev.IsDelete() {
+					break Watcher_Loop
+				}
+				if !ev.IsModify() {
+					continue
+				}
+
+				read, err = readFile.Read(buf)
+				if 0 == read {
+					continue
+				}
+
+				written, err = conn.Write(buf[:read])
+				if nil != err {
+					logger.Printf("error writing to client connection: %s\n", err.Error())
+					break Watcher_Loop
+				}
+				atomic.AddInt64(&wpcli.BytesStreamed, int64(written))
+			case err := <-watcher.Error:
+				logger.Printf("error scanning the logfile: %s", err.Error())
+			}
+		}
+
+		logger.Println("closing connection at the end of the file read")
+		conn.Close()
+	}()
+
+	err = watcher.Watch(wpcli.LogFileName)
+	if err != nil {
+		logger.Printf("error watching the logfile: %s", err.Error())
+		conn.Write([]byte("unable to open the remote process log"))
+		conn.Close()
+		return errors.New(fmt.Sprintf("error watching the logfile: %s\n", err.Error()))
+	}
+
+	processTCPConnectionData(conn, wpcli)
+	conn.Close()
+	connectionActive = false
+	return nil
+}
+
 func runWpCliCmdRemote(conn *net.TCPConn, Guid string, rows uint16, cols uint16, wpCliCmdString string) error {
 	cmdArgs := make([]string, 0)
 	cmdArgs = append(cmdArgs, strings.Fields("--path="+wpPath)...)
@@ -238,7 +453,6 @@ func runWpCliCmdRemote(conn *net.TCPConn, Guid string, rows uint16, cols uint16,
 	cmdArgs = append(cmdArgs, cleanArgs...)
 
 	cmd := exec.Command("/usr/local/bin/wp", cmdArgs...)
-
 	cmd.Env = append(os.Environ(), "TERM=xterm-256color")
 
 	logger.Printf("launching %s - rows: %d, cols: %d, args: %s\n", Guid, rows, cols, strings.Join(cmdArgs, " "))
@@ -256,13 +470,20 @@ func runWpCliCmdRemote(conn *net.TCPConn, Guid string, rows uint16, cols uint16,
 		os.Remove(logFileName)
 	}
 
-	logger.Printf("Opening logfile %s", logFileName)
+	logger.Printf("Creating the logfile %s", logFileName)
 	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0666)
-
 	if nil != err {
 		conn.Write([]byte("unable to launch the remote WP CLI process: " + err.Error()))
 		conn.Close()
-		return errors.New(fmt.Sprintf("error launching the WP CLI process: %s\n", err.Error()))
+		return errors.New(fmt.Sprintf("error creating the WP CLI log file: %s\n", err.Error()))
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		conn.Write([]byte("unable to launch the remote WP CLI process: " + err.Error()))
+		logFile.Close()
+		conn.Close()
+		return errors.New(fmt.Sprintf("error launching the WP CLI log file watcher: %s\n", err.Error()))
 	}
 
 	tty, err := pty.StartWithSize(cmd, &pty.Winsize{Rows: rows, Cols: cols})
@@ -277,9 +498,19 @@ func runWpCliCmdRemote(conn *net.TCPConn, Guid string, rows uint16, cols uint16,
 		cmd.Process.Kill()
 		cmd.Process.Wait()
 		tty.Close()
-		logFile.Close()
-		conn.Close()
 	}()
+
+	padlock.Lock()
+	wpcli := &WpCliProcess{}
+	wpcli.Guid = Guid
+	wpcli.Cmd = cmd
+	wpcli.BytesLogged = 0
+	wpcli.BytesStreamed = 0
+	wpcli.Tty = tty
+	wpcli.LogFileName = logFileName
+	wpcli.Running = true
+	gGUIDttys[Guid] = wpcli
+	padlock.Unlock()
 
 	prevState, err := terminal.MakeRaw(int(tty.Fd()))
 	if nil != err {
@@ -289,142 +520,137 @@ func runWpCliCmdRemote(conn *net.TCPConn, Guid string, rows uint16, cols uint16,
 	}
 	defer func() { _ = terminal.Restore(int(tty.Fd()), prevState) }()
 
-	var bytesRead int = 0
-	var bytesStreamed int = 0
-
 	go func() {
 		var written, read int
-		var err, errTty error
-		var buf []byte = make([]byte, 4096)
+		var buf []byte = make([]byte, 8192)
 
+		readFile, err := os.OpenFile(logFileName, os.O_RDONLY, os.ModeCharDevice)
+		if nil != err {
+			logger.Printf("error opening the read file for the stream: %s\n", err.Error())
+			conn.Close()
+			return
+		}
+
+	Exit_Loop:
 		for {
-			read, errTty = tty.Read(buf)
-			bytesRead += read
-			if 0 < read {
-				padlock.Lock()
-				written, err = logFile.Write(buf[:read])
-				bytesStreamed += written
-				padlock.Unlock()
+			select {
+			case ev := <-watcher.Event:
+				if ev.IsDelete() {
+					break Exit_Loop
+				}
+				if !ev.IsModify() {
+					continue
+				}
 
+				read, err = readFile.Read(buf)
 				if nil != err {
-					logger.Printf("error writing to the local logfile: %s\n", err.Error())
+					logger.Printf("error reading the log file: %s\n", err.Error())
+					continue
+				}
+				if 0 == read {
+					continue
+				}
+
+				if nil == conn {
+					logger.Println("client connection is closed, exiting this watcher loop")
+					break Exit_Loop
 				}
 
 				written, err = conn.Write(buf[:read])
+				atomic.AddInt64(&wpcli.BytesStreamed, int64(written))
+
 				if nil != err {
 					logger.Printf("error writing to client connection: %s\n", err.Error())
-					break
+					break Exit_Loop
 				}
-				if written != read {
-					logger.Printf("error writing to client connection: %s\n", err.Error())
-					break
-				}
-			}
 
-			if nil != errTty {
-				if io.EOF != errTty {
-					logger.Printf("error reading WP CLI stdout: %s\n", errTty.Error())
-				}
-				break
+			case err := <-watcher.Error:
+				logger.Printf("error scanning the logfile %s: %s", logFileName, err.Error())
 			}
+		}
+
+		if nil != conn {
+			logger.Println("closing the connection on exit of the file read")
+			conn.Close()
 		}
 	}()
 
-	go func() {
-		data := make([]byte, 4096)
-		var size, written int
-		var err error
+	err = watcher.Watch(logFileName)
+	if err != nil {
+		logger.Fatal(err)
+	}
 
-		conn.SetReadBuffer(4096)
+	go func() {
+		var written, read int
+		var err error
+		var buf []byte = make([]byte, 8192)
 
 		for {
-			conn.SetDeadline(time.Now().Add(time.Duration(600 * time.Second.Nanoseconds())))
-			size, err = conn.Read(data)
-
-			if nil != err {
-				if io.EOF == err {
-					logger.Println("WP CLI client connection closed")
-					conn.Close()
-				} else {
-					logger.Printf("error reading from the WP CLI client: %s\n", err.Error())
-				}
+			if _, err = tty.Stat(); nil != err {
+				// This is because the command has been terminated
 				break
 			}
+			read, err = tty.Read(buf)
+			atomic.AddInt64(&wpcli.BytesLogged, int64(read))
 
-			if 0 == size {
-				logger.Println("ignoring data of length 0")
-				continue
-			}
-
-			if 1 == size && 0x3 == data[0] {
-				logger.Println("Ctrl-C received, exiting")
-				break
-			}
-
-			if 4 < len(data) && "\xc2\x9b8;" == string(data[:4]) && 't' == data[size-1:][0] {
-				cmdParts := strings.Split(string(data[4:size]), ";")
-
-				rows, err := strconv.ParseUint(cmdParts[0], 10, 16)
-				if nil != err {
-					logger.Printf("error reading rows resize data from the WP CLI client: %s\n", err.Error())
-					continue
-				}
-				cols, err := strconv.ParseUint(cmdParts[1][:len(cmdParts[1])-1], 10, 16)
-				if nil != err {
-					logger.Printf("error reading columns resize data from the WP CLI client: %s\n", err.Error())
-					continue
-				}
-
-				err = pty.Setsize(tty, &pty.Winsize{Rows: uint16(rows), Cols: uint16(cols)})
-				if nil != err {
-					logger.Printf("error performing window resize: %s\n", err.Error())
-				} else {
-					logger.Printf("set new window size: %dx%d\n", rows, cols)
-				}
-				continue
-			}
-
-			padlock.Lock()
-			written, err = logFile.Write(data[:size])
-			padlock.Unlock()
 			if nil != err {
-				logger.Printf("error writing to the local logfile: %s\n", err.Error())
-			}
-
-			written, err = tty.Write(data[:size])
-			if nil != err {
-				if io.ErrClosedPipe == err {
-					logger.Println("error reading from the WP CLI client: closed pipe")
-					break
-				}
-				if io.ErrUnexpectedEOF == err {
-					logger.Println("error reading from the WP CLI client: unexpected EOF")
-					break
-				}
 				if io.EOF != err {
-					panic(err)
+					logger.Printf("error reading WP CLI tty output: %s\n", err.Error())
 				}
+				break
 			}
-			if written != size {
-				logger.Println("error writing to WP CLI client: not enough data written")
+
+			if 0 == read {
+				continue
+			}
+
+			written, err = logFile.Write(buf[:read])
+			if nil != err {
+				logger.Printf("error writing to logfle: %s\n", err.Error())
+				break
+			}
+			if written != read {
+				logger.Printf("error writing to logfile, read %d and only wrote %d\n", read, written)
 				break
 			}
 		}
+		logFile.Close()
+	}()
 
-		logger.Println("terminating the wp command")
-		if nil != cmd.Process {
-			cmd.Process.Kill()
-		}
+	go func() {
+		processTCPConnectionData(conn, wpcli)
+
+		conn.Close()
+		conn = nil
+		logger.Printf("%+v\n", wpcli)
 	}()
 
 	cmd.Process.Wait()
 
-	for {
-		if bytesStreamed >= bytesRead || nil == conn {
-			break
-		}
-		time.Sleep(time.Duration(50 * time.Millisecond.Nanoseconds()))
+	padlock.Lock()
+	wpcli.Running = false
+	padlock.Unlock()
+
+	if nil != cmd.Process {
+		logger.Println("terminating the wp command")
+		cmd.Process.Kill()
 	}
 
+	for {
+		if wpcli.BytesStreamed >= wpcli.BytesLogged || nil == conn {
+			break
+		}
+		time.Sleep(time.Duration(1 * time.Second.Nanoseconds()))
+		logger.Printf("waiting for remaining bytes to be written to a client: at %d - have %d\n", wpcli.BytesStreamed, wpcli.BytesLogged)
+	}
+
+	padlock.Lock()
+	delete(gGUIDttys, Guid)
+	padlock.Unlock()
+
+	if nil != conn {
+		logger.Println("closing the connection at the end")
+		conn.Close()
+	}
 	return nil
 }
