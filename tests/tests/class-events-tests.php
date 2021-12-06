6643a8f7ddc2dfd24da02614cec6fa73dbddd053
Caleb Burks <caleb.burks@automattic.com>
2021-12-06 05:49:16 -0600

    Add methods to the Events class

--- tests/tests/class-events-tests.php
+++ tests/tests/class-events-tests.php
@@ -5,6 +5,7 @@
 
 namespace Automattic\WP\Cron_Control\Tests;
 use Automattic\WP\Cron_Control\Events;
+use Automattic\WP\Cron_Control\Event;
 
 class Events_Tests extends \WP_UnitTestCase {
 	private $registered_actions = [];
@@ -23,6 +24,75 @@ class Events_Tests extends \WP_UnitTestCase {
 
 		_set_cron_array( array() );
 		parent::tearDown();
+	}
+
+	// The actual query functionality is largely tested in the data store already, so here we just ensure the returns are as expected.
+	function test_query() {
+		// Create 2 test events.
+		$this->register_test_event( [ 'action' => 'test_query_action', 'args' => [ 'first' ], 'timestamp' => 1 ] );
+		$this->register_test_event( [ 'action' => 'test_query_action', 'args' => [ 'second' ], 'timestamp' => 2 ] );
+
+		// Ensure both are returned
+		$events = Events::query( [ 'action' => 'test_query_action', 'limit' => 100 ] );
+		$this->assertEquals( count( $events ), 2, 'Correct number of events returned' );
+		$this->assertEquals( $events[0]->get_args(), [ 'first' ], 'Oldest event returned first by default' );
+		$this->assertEquals( $events[1]->get_args(), [ 'second' ], 'Correct second event also found.' );
+
+		// Empty array when none are found.
+		$events = Events::query( [ 'action' => 'non_existent_action', 'limit' => 100 ] );
+		$this->assertEquals( $events, [], 'Returns empty array when no results found' );
+	}
+
+	function test_format_events_for_wp() {
+		$events = $this->create_test_events();
+
+		$expected_format = [
+			$events['A']->get_timestamp() => [ // A & B & C share timestamps
+				$events['A']->get_action() => [ // A & B share action
+					$events['A']->get_instance() => [
+						'schedule' => false,
+						'args' => [],
+					],
+					$events['B']->get_instance() => [
+						'schedule' => 'hourly',
+						'interval' => HOUR_IN_SECONDS,
+						'args' => [ 'B' ],
+					],
+				],
+				$events['C']->get_action() => [ // C has it's own action
+					$events['C']->get_instance() => [
+						'schedule' => false,
+						'args' => [ 'C' ],
+					],
+				],
+			],
+			$events['D']->get_timestamp() => [ // D is on it's own since the timestamp is unique.
+				$events['D']->get_action() => [
+					$events['D']->get_instance() => [
+						'schedule' => false,
+						'args' => [ 'D' ],
+					],
+				],
+			],
+		];
+
+		$formatted = Events::format_events_for_wp( array_values( $events ) );
+		$this->assertEquals( $formatted, $expected_format, 'Returns the correct array format' );
+
+		$empty_formatted = Events::format_events_for_wp( [] );
+		$this->assertEquals( $empty_formatted, [], 'Returns empty array when no events to format' );
+	}
+
+	function test_flatten_wp_events_array() {
+		// Setup an events array the way WP gives it to us.
+		$events = $this->create_test_events();
+		$formatted = Events::format_events_for_wp( array_values( $events ) );
+		$formatted['version'] = 2;
+
+		// Ensure we flatten it w/ all events accounted for.
+		$flattened = Events::flatten_wp_events_array( $formatted );
+		$this->assertEquals( count( $flattened ), 4, 'Returns all expected events' );
+		// Could maybe test more here, but honestly feels like it would couple too closely to the implemention itself.
 	}
 
 	function test_get_events() {
@@ -79,7 +149,7 @@ class Events_Tests extends \WP_UnitTestCase {
 	}
 
 	private function register_test_event( $args = [] ) {
-		$event = wp_parse_args( $args, [ 'timestamp' => time(), 'action' => 'test_event' ] );
+		$event = wp_parse_args( $args, [ 'timestamp' => time(), 'action' => 'test_event', 'args' => [] ] );
 
 		// Easier testing comparision.
 		$event['hashed'] = md5( $event['action'] );
@@ -88,8 +158,49 @@ class Events_Tests extends \WP_UnitTestCase {
 		$this->registered_actions[] = $event['action'];
 		add_action( $event['action'], '__return_true' );
 
-		wp_schedule_single_event( $event['timestamp'], $event['action'] );
+		wp_schedule_single_event( $event['timestamp'], $event['action'], $event['args'] );
 
 		return $event;
+	}
+
+	private function create_test_events() {
+		$time_one = 1400000000;
+		$time_two = 1500000000;
+
+		$events_to_create = [
+			'A' => [
+				'action' => 'test_format_events_for_wp',
+				'args' => [],
+				'timestamp' => $time_one,
+			],
+			'B' => [
+				'action' => 'test_format_events_for_wp',
+				'args' => [ 'B' ],
+				'timestamp' => $time_one,
+				'schedule' => 'hourly',
+				'interval' => HOUR_IN_SECONDS,
+			],
+			'C' => [
+				'action' => 'test_format_events_for_wp_two',
+				'args' => [ 'C' ],
+				'timestamp' => $time_one,
+			],
+			'D' => [
+				'action' => 'test_format_events_for_wp',
+				'args' => [ 'D' ],
+				'timestamp' => $time_two,
+			],
+		];
+
+		$events = [];
+		foreach ( $events_to_create as $event_key => $event_args ) {
+			$test_event = new Event();
+			Utils::apply_event_props( $test_event, $event_args );
+			$test_event->save();
+
+			$events[ $event_key ] = $test_event;
+		}
+
+		return $events;
 	}
 }
